c161ff27fcf5d444f1bbd6e338627747c4566f00
SHMT653 <luca@schmut.de>
2025-10-08 11:51:33 +0200

    neu

--- code-vorlesung/vor-07-10-25/Ratespiel/main.go
+++ code-vorlesung/vor-07-10-25/Ratespiel/main.go
@@ -1,1 +1,35 @@
-package ratespiel
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func ReadNumber() int {
+	fmt.Print("Rate eine Zahl: ")
+	var zahl int
+	fmt.Scan(&zahl)
+	return zahl
+}
+
+func GuessingGame() {
+	my_number := rand.Intn(101) - 50
+	for n := 0; n < 3; n++ {
+		guess := ReadNumber()
+		if Random(guess, my_number) {
+			fmt.Println("Richtig geraten)")
+			return
+		} else {
+			fmt.Println("Das ist leider die Falsche Zahl")
+		}
+	}
+	fmt.Println("Zu viele falsche Zahlen")
+}
+
+func Random(g, e int) bool {
+	return g == e
+}
+
+func main() {
+	GuessingGame()
+}
