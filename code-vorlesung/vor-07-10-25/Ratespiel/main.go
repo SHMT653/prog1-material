ad89cfce8f3665f9b54ec8da70c72d2207cc8ca9
SHMT653 <luca@schmut.de>
2025-10-09 15:17:02 +0200

    fertig

--- code-vorlesung/vor-07-10-25/Ratespiel/main.go
+++ code-vorlesung/vor-07-10-25/Ratespiel/main.go
@@ -14,20 +14,20 @@ func main() {
 func NumberInput() int {
 	var zahl int
 	fmt.Print("Rate eine Zahl zwischen 0-20: ")
-	fmt.Scan(&zahl)
+	fmt.Scan(&zahl) // & zahl übergibt die Speicheradresse, Scan schreibt hierhin
 	return zahl
 }
 
 // Guessing ist das Spiel.
 func Guessing() {
-	random := rand.Intn(21)
-	for n := 0; n < 3; n++ {
-		guess := NumberInput()
-		if Check(guess, random) {
-			fmt.Println("Richtig geraten)")
-			return
+	random := rand.Intn(21)  // Intn(21) liefert eine Zahl von 0 bis 20 (21 exklusiv)
+	for n := 0; n < 3; n++ { // drei Versuche: n = 0,1,2
+		guess := NumberInput()    // liest Zahl vom Nutzer
+		if Check(guess, random) { // true wenn geraten == random
+			fmt.Println("Richtig geraten)") // Erfolgsmeldung
+			return                          // Funktion sofort beenden (Spiel vorbei)
 		} else {
-			fmt.Println(guess, "war leider die falsche Zahl. ")
+			fmt.Println(guess, "war leider die falsche Zahl. ") // mehrere Argumente werden mit Leerzeichen ausgegeben
 		}
 	}
 	fmt.Println("Zu viele falsche Versuche, die richtige Zahl wäre", random, "gewesen")
