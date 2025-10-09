431378e71f7382eb7f200f80f56ca826d737cbb6
SHMT653 <luca@schmut.de>
2025-10-09 15:21:54 +0200

    fertig

--- code-vorlesung/vor-07-10-25/Ratespiel/main.go
+++ code-vorlesung/vor-07-10-25/Ratespiel/main.go
@@ -14,26 +14,26 @@ func main() {
 func NumberInput() int {
 	var zahl int
 	fmt.Print("Rate eine Zahl zwischen 0-20: ")
-	fmt.Scan(&zahl) // & zahl übergibt die Speicheradresse, Scan schreibt hierhin
+	fmt.Scan(&zahl) // Adresse von zahl übergeben (&)
 	return zahl
 }
 
 // Guessing ist das Spiel.
 func Guessing() {
-	random := rand.Intn(21)  // Intn(21) liefert eine Zahl von 0 bis 20 (21 exklusiv)
-	for n := 0; n < 3; n++ { // drei Versuche: n = 0,1,2
-		guess := NumberInput()    // liest Zahl vom Nutzer
-		if Check(guess, random) { // true wenn geraten == random
-			fmt.Println("Richtig geraten)") // Erfolgsmeldung
-			return                          // Funktion sofort beenden (Spiel vorbei)
+	random := rand.Intn(21)
+	for n := 0; n < 3; n++ { // schleife für drei Versuche
+		guess := NumberInput()
+		if Check(guess, random) {
+			fmt.Println("Richtig geraten)")
+			return
 		} else {
-			fmt.Println(guess, "war leider die falsche Zahl. ") // mehrere Argumente werden mit Leerzeichen ausgegeben
+			fmt.Println(guess, "war leider die falsche Zahl. ")
 		}
 	}
 	fmt.Println("Zu viele falsche Versuche, die richtige Zahl wäre", random, "gewesen")
 }
 
 // Check überprüft ob der Guesse und die Random Zahl gleich sind und gibt true or false aus (bool)
-func Check(g, r int) bool {
-	return g == r
+func Check(g, r int) bool { // g = guess, r = random
+	return g == r // true wenn gleich, sonst false
 }
